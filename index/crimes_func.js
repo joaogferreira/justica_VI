a719c1f5c89f2e83dc3788cdafb363bc1f5fb09d
joaogferreira <jgmpof@ua.pt>
2020-11-24 00:09:43 +0000

    pt, cont, ac, mad

--- index/crimes_func.js
+++ index/crimes_func.js
@@ -1,4 +1,4 @@
-var portugal = "pt";
+var portugal = "portugal";
 var continente = "continente";
 var acores = "acores";
 var madeira = "madeira";
@@ -6,7 +6,22 @@ var madeira = "madeira";
 
 // function that wraps around the d3 pattern (bind, add, update, remove)
 function updateLegend(newData) {
-    console.log(newData);
+    if(newData=="portugal"){
+        //Exibir dados de Portugal
+        console.log(true);
+    }
+    else if (newData=="continente"){
+        //Exibir dadops de Continente
+        console.log(true);
+    }
+    else if (newData=="acores"){
+        //Exibir dados de Açores
+        console.log(true);
+    }
+    else if(newData=="madeira"){
+        //Exibir dados de Madeira
+        console.log(true);
+    }
 }
 
 
